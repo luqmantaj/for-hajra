7243df77b20684def1b9a926231e399b4039d0ae
luqmantaj <lastizmtaj@gmail.com>
2026-03-05 05:35:03 +0530

    activate player panel

--- script.js
+++ script.js
@@ -20,13 +20,12 @@ heartsContainer.appendChild(heart);
 function playSong(songPath){
 
 const player = document.getElementById("player");
+const panel = document.getElementById("playerPanel");
 
 player.src = songPath;
-
 player.load();
+player.play();
 
-player.play().catch(function(error){
-console.log("Playback blocked:", error);
-});
+panel.classList.add("active");
 
 }
